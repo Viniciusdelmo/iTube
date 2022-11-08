1e826768d92e8f0378aff503b0b2b62023e91d71
Vinícius Delmo <vinicius.delmo@gmail.com>
2022-11-08 15:28:51 -0300

    Atualização aula2

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,40 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu";
+import Menu from "../src/components/Menu/index";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estiloDaHomePage = { 
-    //backgroundColor: "red" 
+  const estiloDaHomePage = {
+    //backgroundColor: "red"
   };
-
   //console.log(config.playlists);
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
   return (
     <>
-        <CSSReset />
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            flex: 1,
-            // backgroundColor: "red",
-        }}>
-            <Menu />
-            <Header />
-            <Timeline playlists={config.playlists}>
-                Conteúdo
-            </Timeline>
-        </div>
+      <CSSReset />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          // backgroundColor: "red",
+        }}
+      >
+        <Menu
+          valorDoFiltro={valorDoFiltro}
+          setValorDoFiltro={setValorDoFiltro}
+        />
+        <Header />
+        <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+          Conteúdo
+        </Timeline>
+      </div>
     </>
-);
+  );
 }
-
 
 export default HomePage;
 
@@ -44,7 +49,6 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -52,26 +56,27 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+const StyledBanner = styled.div`
+background-image: url(${config.bg}) ;
+height: 230px;
+`;
 function Header() {
   return (
     <StyledHeader>
+      <StyledBanner/>
       {/* <img src="banner" /> */}
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
-          <h2>
-            {config.name}
-          </h2>
-          <p>
-            {config.job}
-          </p>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
         </div>
       </section>
     </StyledHeader>
   );
 }
 
-function Timeline(propriedades) {
+function Timeline({ searchValue, ...propriedades }) {
   //console.log("dentro do componente", propriedades.playlists);
   const playlistNames = Object.keys(propriedades.playlists);
   //statement
@@ -82,19 +87,23 @@ function Timeline(propriedades) {
         const videos = propriedades.playlists[playlistName];
         console.log(videos);
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>
-                        {video.title}
-                    </span>
-                  </a>
-                );
-              })}
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
+                  return titleNormalized.includes(searchValueNormalized);
+                })
+                .map((video) => {
+                  return (
+                    <a href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })}
             </div>
           </section>
         );
